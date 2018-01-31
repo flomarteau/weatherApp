920be3733ccf58f99cb9d6e06f05dd7785c4f5a8
flora <flora.marteau@live.fr>
2018-01-31 17:34:55 +0100

    ajout de l'api openweatherapp et de la notion de bdd

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,89 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var mongoose= require('mongoose');
 
-// On part du principe que la liste est un tableau vide
-var tableville = [];
+// On fait le lien entre le document js et la bdd mlab en utilisant le nom du compte, mdp, adresse, et nom de la bdd
+var options = { server: { socketOptions: {connectTimeoutMS: 5000 } }};
+mongoose.connect('mongodb://Flora:mlab2807@ds219318.mlab.com:19318/openweatherapp',
+    options,
+    function(err) {
+     console.log(err);
+    }
+);
 
-// Création d'un tableau comprenant toutes les villes
-var tableville = [
-  "Paris", "Lyon", "Lille", "Marseille", "Bordeaux", "Ajaccio"
-];
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Weatherapp', tableville });
+// Définition du schéma : types de données de l'enregistrement
+var citySchema = mongoose.Schema({
+  name: String,
+  tempmin: Number,
+  tempmax: Number,
+  temps: String,
+  picto: String,
+  long: Number,
+  lat: Number
 });
 
+// Création du modèle (qui permet de lire, écrire, modifier et supprimer les enregistrements)
+var cityModel = mongoose.model('openweatherapp', citySchema);
+
+
+router.get('/', function(req, res, next) {
+
+  cityModel.find({}, function(error, city) {
+    if (error) throw error;
+    console.log(city);
+    res.render('index', { city });
+  });
+
+});
+
+
 router.post('/addville', function(req, res, next) {
-    if (req.body.ville !== "") {
-      tableville.push(req.body.ville);
-      res.render('index', { tableville });
-    } else {
-      res.render('index', { tableville });
-    };
+  request("http://api.openweathermap.org/data/2.5/weather?q="+req.body.ville+"&lang=fr&units=metric&APPID=7641873b9fca43b3f95efab8fa923609",
+  function(error, response, body) {
+
+    body = JSON.parse(body);
+    console.log(typeof(body.main.temp_min));
+
+    var newCity = new cityModel ({
+      name: body.name,
+      tempmin: body.main.temp_min,
+      tempmax: body.main.temp_max,
+      temps: body.weather[0].description,
+      picto: body.weather[0].icon,
+      long: body.coord.lon,
+      lat: body.coord.lat
+    });
+
+    newCity.save(function (error, user) {
+
+      cityModel.find({}, function(error, city) {
+        if (error) throw error;
+        console.log(city);
+        res.render('index', { city });
+      });
+    });
+  });
 });
 
 router.get('/deleteville', function(req, res, next) {
-  tableville.splice(req.query.position, 1);
-  res.render('index', { tableville });
+
+    cityModel.remove (
+      {_id: req.query.id},
+      function(error) {
+        console.log(error)
+      }
+    );
+
+  cityModel.find({}, function(error, city) {
+    if (error) throw error;
+    res.render('index', { city });
+  });
+
 });
+
+
+
 
 
 module.exports = router;
